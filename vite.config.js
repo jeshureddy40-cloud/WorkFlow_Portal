4e888513f92ee8c408bcfc503d8e0c2720413ee3
Jeswanth reddy <jeshureddy40@gmail.com>
2026-02-28 15:53:50 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  // GitHub Pages serves this project from /WorkFlow_Portal/.
+  base: command === 'serve' ? '/' : '/WorkFlow_Portal/',
+}))
